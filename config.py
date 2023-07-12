ffaf826e7e0b53e2c528d9c4c80303f83cc6b3e7
statkevich_vv <vikvikst474@gmail.com>
2023-07-12 17:10:43 +0700

    Set start config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'temp_keydsfdfdf3489%^&'
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345@localhost/db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
